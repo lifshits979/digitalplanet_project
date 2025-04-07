2d94d1166910feea00b18f81f876d2ac38d2728a
Lifshits979 <li979@ya.ru>
2025-04-07 18:32:35 +0300

    верстка моего проекта

---
+++ js/digitalplanet.js
@@ -0,0 +1,213 @@
+let page = document.querySelector('.page');
+let body = document.querySelector('body');
+
+let logos = document.querySelectorAll('.logo');
+let logoregs = document.querySelectorAll('.logoreg');
+let logohovers = document.querySelectorAll('.logohover');
+
+logos.forEach((logo) => {
+    logo.addEventListener('mouseover', () => {
+        logoregs.forEach((logoreg) => {
+            logoreg.style.display = 'none';
+        })
+    logohovers.forEach((logohover) => {
+        logohover.style.display = 'block';
+    });
+});
+
+    logo.addEventListener('mouseout', () => {
+        logohovers.forEach((logohover) => {
+            logohover.style.display = 'none';
+        });
+        logoregs.forEach((logoreg) => {
+            logoreg.style.display = 'block';
+        });
+});
+});
+
+let headers = document.querySelectorAll('header');
+let buttons = document.querySelectorAll('.page-title-button');
+let aboutButtons = document.querySelectorAll('.about-button');
+let howButtons = document.querySelectorAll('.how-button');
+let techButtons = document.querySelectorAll('.tech-button');
+let QAButtons = document.querySelectorAll('.QA-button');
+let contactsButtons = document.querySelectorAll('.contacts-button')
+
+for (let aboutButton of aboutButtons){
+    aboutButton.onclick = function(){
+        window.scrollTo(0, 768);
+    }
+}
+
+for (let howButton of howButtons){
+    howButton.onclick = function(){
+        window.scrollTo(0, 768*2);
+    }
+}
+
+for (let techButton of techButtons){
+    techButton.onclick = function(){
+        window.scrollTo(0, 768*3);
+    }
+}
+
+for (let QAButton of QAButtons){
+    QAButton.onclick = function(){
+        window.scrollTo(0, 768*4);
+    }
+}
+
+for (let contactsButton of contactsButtons){ 
+    contactsButton.onclick = function(){
+        window.scrollTo(0, 768*5);
+    }
+}
+
+let MPScroll = document.querySelector('.main-page-scroll');
+MPScroll.onclick = function(){
+    window.scrollTo(0, 768)
+}
+
+let scrollButtons = document.querySelectorAll('.scroll');
+for (let scroll of scrollButtons){
+    scroll.onclick = function(){
+        window.scrollTo(0, pageYOffset+768)
+    }
+}
+
+let switchForward = document.querySelector('.gallery-switch-forward');
+let switchBack = document.querySelector('.gallery-switch-back');
+let galleryImages = document.querySelectorAll('.gallery-image');
+let switchCount = document.querySelector('.gallery-switch-count');
+let switchbackreg = document.querySelector('.switchbackreg');
+let switchbackhover = document.querySelector('.switchbackhover');
+let switchforwardreg = document.querySelector('.switchforwardreg');
+let switchforwardhover = document.querySelector('.switchforwardhover');
+let currentIndex=0;
+
+function showImage(index) {
+    galleryImages.forEach((galleryImage, i) => {
+        if (i === index) {
+            galleryImage.classList.remove('gallery-hidden');
+            galleryImage.classList.add('gallery-shown');
+        } else {
+            galleryImage.classList.remove('gallery-shown');
+            galleryImage.classList.add('gallery-hidden');
+        }
+    });
+};
+
+switchForward.addEventListener('mouseover', () => {
+    switchforwardreg.style.display = 'none';
+    switchforwardhover.style.display = 'block';
+});
+switchForward.addEventListener('mouseout', () => {
+    switchforwardhover.style.display = 'none';
+    switchforwardreg.style.display = 'block';
+});
+switchBack.addEventListener('mouseover', () => {
+    switchbackreg.style.display = 'none';
+    switchbackhover.style.display = 'block';
+});
+switchBack.addEventListener('mouseout', () => {
+    switchbackhover.style.display = 'none';
+    switchbackreg.style.display = 'block';
+});
+
+
+
+switchForward.onclick = function () {
+    if (currentIndex < galleryImages.length - 1) {
+        currentIndex++;
+        showImage(currentIndex);
+        switchCount.textContent = ' '+'0'+(currentIndex+1)+' '+'/';
+    }
+};
+switchBack.onclick = function () {
+    if (currentIndex > 0) {
+        currentIndex--;
+        showImage(currentIndex);
+        switchCount.textContent = ' '+'0'+(currentIndex+1)+' '+'/';
+    }
+};
+
+
+
+let answerButtons = document.querySelectorAll('.plus-pic');
+let answers = document.querySelectorAll('.answer');
+let openSymbols = document.querySelectorAll('.plus-pic p')
+let QAFooter = document.querySelector('.QA-footer');
+
+answerButtons.forEach((answerButton, index) => {
+    answerButton.onclick = function (){
+        if (answers[index].classList.contains('answer-hidden')){
+            answers.forEach((answer) => {
+                answer.style.display = 'none';
+                answer.classList.add('answer-hidden')});
+            openSymbols.forEach(openSymbol => {
+                openSymbol.style.transform = 'rotate(0deg)'
+            });
+            answers[index].style.display = 'block';
+            answers[index].classList.remove('answer-hidden');
+            let answerHeight = answers[index].clientHeight;
+            QAFooter.style.marginTop = 187 - answerHeight + 'px';
+            openSymbols[index].style.transform = 'rotate(45deg)';
+        }
+        else {answers[index].style.display = 'none';
+            answers[index].classList.add('answer-hidden');
+            openSymbols[index].style.transform = 'rotate(0deg)'
+            QAFooter.style.marginTop = 187 + 'px';
+        }
+    };
+});
+
+let headerOrderButtons = document.querySelectorAll('.header-order');
+let mainOrder = document.querySelector('.main-button-order');
+let orderField = document.querySelector('.order');
+let orderCloseButton = document.querySelector('.order-close-button');
+let sendButtonInOrder = document.querySelector('.submit-order');
+let orderForm = document.querySelector('.order-form');
+let questionsForm = document.querySelector('.questions-form');
+
+function openOrder(){
+    orderField.style.display = 'block';
+    page.style.filter = 'brightness(0.2)';
+};
+function closeOrder(){
+    orderField.style.display = 'none';
+    page.style.filter = 'none';
+};
+
+headerOrderButtons.forEach((headerOrderButton) => {
+    headerOrderButton.onclick = openOrder
+});
+mainOrder.onclick = openOrder;
+orderCloseButton.onclick =closeOrder;
+
+let success = document.querySelector('.success');
+let sendButton = document.querySelector('.submit-question');
+let successOKButton = document.querySelector('.success-ok');
+let successCloseButton = document.querySelector('.success-close-button')
+
+function openSuccess(){
+    success.style.display = 'block';
+    page.style.filter = 'brightness(0.2)';
+}
+
+function closeSuccess(){
+    success.style.display = 'none';
+    page.style.filter = 'none';
+}
+
+questionsForm.onsubmit = function(evt){
+    evt.preventDefault();
+    openSuccess();
+}    
+orderForm.onsubmit = function(evt){
+    evt.preventDefault();
+    closeOrder();
+    openSuccess(); 
+}
+successCloseButton.onclick = closeSuccess;
+successOKButton.onclick = closeSuccess;
+
